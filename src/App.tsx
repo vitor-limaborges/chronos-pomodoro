9f8e0407b04a4834cbe325b55988e8b7cfae7320
vitor-limaborges <vitor.borges@weareglobals.com>
2025-08-25 10:51:55 -0300

    Aula 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from './components/Heading'
+import './styles/global.css'
+import './styles/theme.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    // Não é necessario  um elemento pai, pois o react te força a ter um
+    <> 
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.colocar a "div" em si, apenas colocar A debitis
+        animi, corporis inventore odio vel.
       </p>
     </>
-  )
+  );
 }
-
-export default App
